3c9628e6c592cc40f8de8ed318c6e22380072706
nwn8 <s582031@nwmissouri.edu>
2025-03-13 08:16:26 -0500

    update scripts

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # smart-store-nate
+
+Initial analysis of Customers, Products, and Sales Data.
+
+1. What is the Most common Customer location?
+2. Highest/lowest product price?
+3. Estimate:Avg, Min, Max sales?
+4. Any data issues?
--- scripts/initial_analysis.py
+++ scripts/initial_analysis.py
@@ -2,25 +2,49 @@
 import pathlib
 import csv
 import statistics
-from utils import utils_logger
 
 
-fetched_folder_name: str = "data"
-processed_folder_name: str = "data_processed"
 
-def analyze_data(file_path: pathlib.Path) -> dict:
+
+data_folder = "data"
+raw_folder = pathlib.Path(data_folder, "raw")
+customer_data_file = pathlib.Path(raw_folder, "customers_data.csv")
+
+def analyze_Region_data(file_path: pathlib.Path) -> dict:
     """Analyze the data to calculate min, max, mean, and stdev."""
     try:
+    
+        Region_list = []
+        with file_path.open('r') as file:
+          
+            dict_reader = csv.DictReader(file)  
+            for row in dict_reader:
+                try:
+                    Region = row["Region"] 
+            
+                    Region_list.append(Region)
+                except ValueError as e:
+                    logger.warning(f"Skipping invalid row: {row} ({e})")
+        
+      
+        print(Region_list)
+    except Exception as e:
+        logger.error(f"Error processing CSV file: {e}")
+        return {}
+    
+def analyze_data(file_path: pathlib.Path) -> dict:
+    """Analyze the data to calculate min, max, mean, and stdev.
+    try:
         # initialize an empty list to store the scores
-        score_list = []
+        Region_list = []
         with file_path.open('r') as file:
             # csv.DictReader() methods to read into a DictReader so we can access named columns in the csv file
             dict_reader = csv.DictReader(file)  
             for row in dict_reader:
                 try:
-                    score = float(row["Obesity"])  # Extract and convert to float
+                    Region = row["Region"] 
                     # append the score to the list
-                    score_list.append(score)
+                    Region_list.append(Region)
                 except ValueError as e:
                     utils_logger.logger.warning(f"Skipping invalid row: {row} ({e})")
         
@@ -35,10 +59,9 @@ def analyze_data(file_path: pathlib.Path) -> dict:
     except Exception as e:
         utils_logger.logger.error(f"Error processing CSV file: {e}")
         return {}
-    
-
+    """
 def process_csv_file():
-    """This will write the stats to data_processed/obesity_stats.txt"""
+    """This will write the stats to data_processed/obesity_stats.txt
     input_file = pathlib.Path(fetched_folder_name, "Obesity.csv")
     output_file = pathlib.Path(processed_folder_name, "obesity_stats.txt")
     
@@ -51,5 +74,6 @@ def process_csv_file():
         file.write(f"Maximum: {stats['max']:.2f}\n")
         file.write(f"Mean: {stats['mean']:.2f}\n")
         file.write(f"Standard Deviation: {stats['stdev']:.2f}\n")
-    
-    logger.info(f"Processed CSV file: {input_file}, Statistics saved to: {output_file}")
+"""
+
+analyze_Region_data(customer_data_file)
