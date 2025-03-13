47b81b9cad329fc5fd145c74f441f59b2364e381
nwn8 <s582031@nwmissouri.edu>
2025-03-13 08:29:57 -0500

    script

--- scripts/initial_analysis.py
+++ scripts/initial_analysis.py
@@ -1,79 +1,22 @@
+import pandas as pd
 
-import pathlib
-import csv
-import statistics
+df_customers = pd.read_csv('data/raw/customers_data.csv')
+df_products = pd.read_csv('data/raw/products_data.csv')
+df_sales= pd.read_csv('data/raw/sales_data.csv')
 
 
+most_common_region_cust = df_customers['Region'].value_counts().idxmax()
+min_value_product = df_products['UnitPrice'].min()
+max_value_product = df_products['UnitPrice'].max()
+min_sales = df_sales['SaleAmount'].min()
+max_sales = df_sales['SaleAmount'].max()
+avg_sales = df_sales['SaleAmount'].mean()
+avg_sales_formatted = "{:.2f}".format(avg_sales)
 
 
-data_folder = "data"
-raw_folder = pathlib.Path(data_folder, "raw")
-customer_data_file = pathlib.Path(raw_folder, "customers_data.csv")
-
-def analyze_Region_data(file_path: pathlib.Path) -> dict:
-    """Analyze the data to calculate min, max, mean, and stdev."""
-    try:
-    
-        Region_list = []
-        with file_path.open('r') as file:
-          
-            dict_reader = csv.DictReader(file)  
-            for row in dict_reader:
-                try:
-                    Region = row["Region"] 
-            
-                    Region_list.append(Region)
-                except ValueError as e:
-                    logger.warning(f"Skipping invalid row: {row} ({e})")
-        
-      
-        print(Region_list)
-    except Exception as e:
-        logger.error(f"Error processing CSV file: {e}")
-        return {}
-    
-def analyze_data(file_path: pathlib.Path) -> dict:
-    """Analyze the data to calculate min, max, mean, and stdev.
-    try:
-        # initialize an empty list to store the scores
-        Region_list = []
-        with file_path.open('r') as file:
-            # csv.DictReader() methods to read into a DictReader so we can access named columns in the csv file
-            dict_reader = csv.DictReader(file)  
-            for row in dict_reader:
-                try:
-                    Region = row["Region"] 
-                    # append the score to the list
-                    Region_list.append(Region)
-                except ValueError as e:
-                    utils_logger.logger.warning(f"Skipping invalid row: {row} ({e})")
-        
-        # Calculate statistics
-        stats = {
-            "min": min(score_list),
-            "max": max(score_list),
-            "mean": statistics.mean(score_list),
-            "stdev": statistics.stdev(score_list) if len(score_list) > 1 else 0,
-        }
-        return stats
-    except Exception as e:
-        utils_logger.logger.error(f"Error processing CSV file: {e}")
-        return {}
-    """
-def process_csv_file():
-    """This will write the stats to data_processed/obesity_stats.txt
-    input_file = pathlib.Path(fetched_folder_name, "Obesity.csv")
-    output_file = pathlib.Path(processed_folder_name, "obesity_stats.txt")
-    
-    stats = analyze_data(input_file)
-    output_file.parent.mkdir(parents=True, exist_ok=True)
-    
-    with output_file.open('w') as file:
-        file.write("Obesity Statistics:\n")
-        file.write(f"Minimum: {stats['min']:.2f}\n")
-        file.write(f"Maximum: {stats['max']:.2f}\n")
-        file.write(f"Mean: {stats['mean']:.2f}\n")
-        file.write(f"Standard Deviation: {stats['stdev']:.2f}\n")
-"""
-
-analyze_Region_data(customer_data_file)
+print(f"Most Common Customer Location: {most_common_region_cust}")
+print(f"Highest Product Price: {max_value_product}")
+print(f"Lowest Product Price: {min_value_product}")
+print(f"Average Sales: {avg_sales_formatted}")
+print(f"Min Sales: {min_sales}")
+print(f"Max Sales: {max_sales}")
